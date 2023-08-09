9fb608de1c6d6739377351f2739e30a1589586d2
mitesh darda <mitesh.darda.0@gmail.com>
2023-08-09 12:51:44 +0530

    fix: router fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
 import Nav from './Navbar/Navbar'
+import { useEffect } from 'react'
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/Home');
+  })
 
   return (
     <>
