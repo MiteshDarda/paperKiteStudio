56dad7e391eecc0d8f525ac120260f6c3bfd8329
mitesh darda <mitesh.darda.0@gmail.com>
2023-08-09 13:01:20 +0530

    fix: make contact us responsive

--- src/ContactUs/ContactUs.tsx
+++ src/ContactUs/ContactUs.tsx
@@ -7,8 +7,8 @@ function ContactUs() {
             <Card shadow="sm" className="p-unit-12 mb-12 w-11/12 min-h-[80vh] flex flex-col justify-center">
                 <div>
                     <Link isExternal href="https://www.instagram.com/studiopaperkites/?utm_medium=copy_link" color="foreground">
-                        <InstagramFilled className=" text-3xl" />
-                        <span className=" text-3xl"> &nbsp; &nbsp;
+                        <InstagramFilled className=" md:text-3xl" />
+                        <span className=" md:text-3xl"> &nbsp; &nbsp;
                             studiopaperkites
                         </span>
                     </Link>
@@ -18,8 +18,8 @@ function ContactUs() {
                 <br></br>
                 <div>
                     <Link isExternal color="foreground" href="mailto:info@studiopaperkites.com">
-                        <MailFilled className=" text-3xl" />
-                        <span className=" text-3xl"> &nbsp; &nbsp;
+                        <MailFilled className=" md:text-3xl" />
+                        <span className=" md:text-3xl"> &nbsp; &nbsp;
                             info@studiopaperkites.com
                         </span>
                     </Link>
@@ -29,8 +29,8 @@ function ContactUs() {
                 <br></br>
                 <div>
                     <Link isExternal color="foreground" href="tel:+91-8890733990">
-                        <PhoneFilled className="text-3xl" />
-                        <span className=" text-3xl"> &nbsp; &nbsp;
+                        <PhoneFilled className="md:text-3xl" />
+                        <span className=" md:text-3xl"> &nbsp; &nbsp;
                             +91-8890733990
                         </span>
                     </Link>
